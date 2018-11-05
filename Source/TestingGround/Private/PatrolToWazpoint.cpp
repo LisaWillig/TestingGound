c2a2d73d721c967b050a79e1c95e964d8ddb9ac2
Unknown <lisaphysik@gmail.com>
2018-11-05 22:42:09 +0100

    Finished WaypointMigration to Cpp

--- Source/TestingGround/Private/PatrolToWazpoint.cpp
+++ Source/TestingGround/Private/PatrolToWazpoint.cpp
@@ -3,14 +3,25 @@
 
 #include "PatrolToWazpoint.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UPatrolToWazpoint::ExecuteTask(UBehaviorTreeComponent& OwnerComponent, uint8* NodeMemory) {
 
-	auto BlackboardComp = OwnerComponent.GetBlackboardComponent();
+	AAIController* AIController = OwnerComponent.GetAIOwner();
+	APawn* ControlledPawn = AIController->GetPawn();
+	APatrollingGuard* PatrollingGuard = Cast <APatrollingGuard > (ControlledPawn);
+	TArray<AActor*> PatrolPoints = PatrollingGuard->WayPoints;
 
-	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+
+	auto BlackboardComp = OwnerComponent.GetBlackboardComponent();
+	int Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WayPointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	UE_LOG(LogTemp, Warning, TEXT("Next Index: %i"), NextIndex)
 	return EBTNodeResult::Succeeded;
 	
 }
